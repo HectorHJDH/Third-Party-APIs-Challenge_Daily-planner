7089b1345637e2715ee3bc7fecda672d33d1edac
HectorJosueDH <hectorjosue78@gmail.com>
2023-04-14 18:09:37 -0600

    Added more time blocks in the HTML from 9am to 5pm, created JS functions that now allows entering text into the time blocks and save them in our local storage by clicking the save button, also the content of these time boxes doesn't change when reloaded.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-
 var weekDay = dayjs().format('dddd');
 var month = dayjs().format('MMMM');
 var day = dayjs().format('D');
@@ -17,9 +16,6 @@ function ordinal(day) {
   }
   return day + "th";
 }
-
-$('#currentDay').text(weekDay + ', ' + month + ' ' + ordinal(day));
-
 /* For ensuring that the code isn't run until the browser has finished rendering all the elements in the html 
 we use function like this: "$()", that is the shorthand for this: $(document).ready(function()) */
 $(function () {
@@ -29,16 +25,33 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+  
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+  
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
+  
   // TODO: Add code to display the current date in the header of the page.
+  $('#currentDay').text(weekDay + ', ' + month + ' ' + ordinal(day));
+
+  const description = document.querySelectorAll('.description');
+  description.forEach((block, i) => {
+    const savedData = localStorage.getItem(`boxData ${i}`);
+    if (savedData) {
+      block.value = savedData;
+    }
+  });
+
+  const saveBtn = document.querySelectorAll('.saveBtn');
+  saveBtn.forEach((btn, i) => {
+    btn.addEventListener('click', function() {
+      const inputTxtArea = description[i].value;
+      localStorage.setItem(`boxData ${i}`, inputTxtArea);
+    });
+  });
 });
